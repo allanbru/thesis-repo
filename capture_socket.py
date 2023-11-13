3e613308276e205bae64e92b3d030f4386995cbb
Allan <44301191+allanbru@users.noreply.github.com>
2023-11-13 13:18:48 -0300

    Screenshoter updated

--- capture_socket.py
+++ capture_socket.py
@@ -5,41 +5,37 @@ class CaptureSocket:
     host = "127.0.0.1"
     port = 9018
     
-    def __init__(self):
-        self.s = None
-        self.start()
-    
-    def start(self):
-        try:
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.s.setblocking(0)
-            self.s.connect((self.host, self.port))
-        except Exception as e:
-            print(e)
-
     def get_screenshot(self, url):
         """
             Returns path to file
         """
         
-        self.start()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(20)
+        s.connect((self.host, self.port))
 
         try:
-            self.s.send(bytearray(url, 'utf-8'))
-            self.s.close()
+            s.send(bytearray(url, 'utf-8'))
+            s.close()
             return False, None            
-        except Exception:
+        except Exception as e:
+            print("[CAPTURE SOCKET] " + str(e))
             return False, None 
  
     def terminate(self):
         """
             Sends termination signal
         """
-        self.start()
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(20)
+        s.connect((self.host, self.port))
+
         try:
-            self.s.send(bytearray("finished", 'utf-8'))
-            result = self.s.recv(1024).decode()
-            self.s.close()
+            s.send(bytearray("finished", 'utf-8'))
+            result = s.recv(1024).decode()
+            s.close()
             return result is not None and result == "Finished"
-        except Exception:
+        except Exception as e:
+            print("[CAPTURE SOCKET] " + str(e))
             return False
